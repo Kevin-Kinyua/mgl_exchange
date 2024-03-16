492aff142a3a984765f66453bb2396c06c6331b5
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2024-03-16 17:42:46 +0300

    Add Eth value

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -11,7 +11,7 @@ const auth = (...roles) => {
             const bearer = 'Bearer ';
 
             if (!authHeader || !authHeader.startsWith(bearer)) {
-                res.send({response:false, message:'Access denied. No credentials sent!', data:null})
+                return res.send({response:false, message:'Access denied. No credentials sent!', data:null})
             }
 
             const token = authHeader.replace(bearer, '');
@@ -22,21 +22,21 @@ const auth = (...roles) => {
             const user = await UserModel.findOne({ id: decoded.user_id });
 
             if (!user) {
-                res.send({response:false, message:'Authentication failed!', data:null})
+                return res.send({response:false, message:'Authentication failed!', data:null})
             }
 
             // check if the current user is the owner user
-            const ownerAuthorized = req.params.id == user.id;
+            const ownerAuthorized = req.params.id === user.id;
             // if the current user is not the owner and
             // if the user role don't have the permission to do this action.
             // the user will get this error
             if (!ownerAuthorized && roles.length && !roles.includes(user.role)) {
-                res.send({response:false, message:'Unauthorized', data:null})
+                return res.send({response:false, message:'Unauthorized', data:null})
             }
 
             // if the user has permissions
-            req.currentUser = user;
-            next();
+            return req.currentUser = user;
+            // next();
 
         } catch (e) {
             e.status = 401;
